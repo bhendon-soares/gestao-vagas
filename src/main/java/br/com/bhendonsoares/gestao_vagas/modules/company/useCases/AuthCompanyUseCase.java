1509a390c8d82b34f4a7894daaf56c9fe688c54b
Bhendon Soares <bhendon.soares12@gmail.com>
2024-08-28 16:59:34 -0300

    feat: add securityfilter and jwtprovider configuration

--- src/main/java/br/com/bhendonsoares/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/bhendonsoares/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @Service
 public class AuthCompanyUseCase {
 
@@ -37,6 +40,7 @@ public class AuthCompanyUseCase {
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         var token = JWT.create().withIssuer("javagas")
+                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(company.getUuid().toString())
                 .sign(algorithm);
 
