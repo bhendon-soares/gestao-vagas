277324fb17e5b04d5c244b781902f0c8c405b6d8
Bhendon Soares <bhendon.soares12@gmail.com>
2024-09-04 16:10:20 -0300

    feat: configure preauthorize to candidate roles

--- src/main/java/br/com/bhendonsoares/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/bhendonsoares/gestao_vagas/security/SecurityCandidateFilter.java
@@ -23,7 +23,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        SecurityContextHolder.getContext().setAuthentication(null);
+//        SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
         if (request.getRequestURI().startsWith("/candidate")) {
@@ -38,7 +38,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
                 request.setAttribute("candidate_id", token.getSubject());
                 var roles = token.getClaim("roles").asList(Object.class);
 
-                var grants = roles.stream().map(role -> new SimpleGrantedAuthority(role.toString())).toList();
+                var grants = roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())).toList();
 
                 UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(), null, grants);
                 SecurityContextHolder.getContext().setAuthentication(auth);
--- src/main/java/br/com/bhendonsoares/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/bhendonsoares/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.bhendonsoares.gestao_vagas.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
